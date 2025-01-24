d0708d97da42ae44fa2a37a8af8f300e88f0d01d
Oliver Po <95318157+0x9u@users.noreply.github.com>
2025-01-24 15:29:22 +1100

    Add roulette and russian roulette (#2)

--- utils/gambling.py
+++ utils/gambling.py
@@ -2,27 +2,29 @@ import hashlib
 import random
 import numpy as np
 
+
 def sum_of_numbers_in_string(input_string):
-  total = 0
-  for char in input_string:
-    if char.isdigit():  # Check if the character is a digit
-      total += int(char)
-  return total
+    total = 0
+    for char in input_string:
+        if char.isdigit():  # Check if the character is a digit
+            total += int(char)
+    return total
+
 
 def slot_gamble(bet: int):
-  slot1 = (sum_of_numbers_in_string(
-    str(hashlib.sha256(str(np.random.randint(1, 100)).encode()).hexdigest())
+    slot1 = (sum_of_numbers_in_string(
+        str(hashlib.sha256(str(np.random.randint(1, 100)).encode()).hexdigest())
     ) + random.random() * 10) % 35
-  
-  slot2 = (sum_of_numbers_in_string(
-    str(hashlib.sha256(str(np.random.randint(1, 100)).encode()).hexdigest())
+
+    slot2 = (sum_of_numbers_in_string(
+        str(hashlib.sha256(str(np.random.randint(1, 100)).encode()).hexdigest())
     ) + random.random() * 10) % 36
-  
-  slot3 = (sum_of_numbers_in_string(
-    str(hashlib.sha256(str(np.random.randint(1, 100)).encode()).hexdigest())
+
+    slot3 = (sum_of_numbers_in_string(
+        str(hashlib.sha256(str(np.random.randint(1, 100)).encode()).hexdigest())
     ) + random.random() * 10) % 36
-      
-  if slot1 <= slot2 and slot2 <= slot3:
-    return (10 * bet)
-  else:
-    return 0
+
+    if slot1 <= slot2 and slot2 <= slot3:
+        return (10 * bet)
+    else:
+        return 0
